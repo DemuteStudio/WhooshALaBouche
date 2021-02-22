3e5d348a81360cfb2f3314fdda06163d5f678d86
ArnaudTorfs <10039@etu.he2b.be>
2021-02-22 17:32:39 +0100

    Update

--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -7,26 +7,30 @@
     This component lives inside our window, and this is where you should put all
     your controls and content.
 */
-class MainComponent  : public juce::AudioAppComponent
+class MainComponent : public juce::AudioAppComponent
 {
 public:
-    //==============================================================================
-    MainComponent();
-    ~MainComponent() override;
+	//==============================================================================
+	MainComponent();
+	~MainComponent() override;
 
-    //==============================================================================
-    void prepareToPlay (int samplesPerBlockExpected, double sampleRate) override;
-    void getNextAudioBlock (const juce::AudioSourceChannelInfo& bufferToFill) override;
-    void releaseResources() override;
+	//==============================================================================
+	void prepareToPlay(int samplesPerBlockExpected, double sampleRate) override;
+	void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill) override;
+	void releaseResources() override;
 
-    //==============================================================================
-    void paint (juce::Graphics& g) override;
-    void resized() override;
+	//==============================================================================
+	void paint(juce::Graphics& g) override;
+	void resized() override;
 
 private:
-    //==============================================================================
-    // Your private member variables go here...
+	juce::Random random;
+	juce::Slider levelSlider;
+	juce::Label levelLabel;
+
+	//==============================================================================
+	// Your private member variables go here...
 
 
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
+	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainComponent)
 };
