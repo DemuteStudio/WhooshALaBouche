44c7a6a7812c11186d3f57da391e319f2a3361a0
ArnaudTorfs <10039@etu.he2b.be>
2021-02-23 06:21:53 +0100

    Output Mic IN
    Software output the microphone input

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -3,42 +3,37 @@
 //==============================================================================
 MainComponent::MainComponent()
 {
-	for (juce::AudioIODeviceType* t : deviceManager.getAvailableDeviceTypes())
+	for (AudioIODeviceType* t : deviceManager.getAvailableDeviceTypes())
 	{
 		t->scanForDevices();
 		auto lst = t->getDeviceNames();
 	};
 
-	levelSlider.setRange(0.0, 0.25);
-	levelSlider.setTextBoxStyle(juce::Slider::TextBoxRight, false, 100, 20);
-	levelLabel.setText("Volume", juce::dontSendNotification);
+	slider_noise_level_.setRange(0.0, 1.0, 0.1);
+	slider_noise_level_.setTextBoxStyle(Slider::TextBoxRight, false, 100, 50);
+	addAndMakeVisible(slider_noise_level_);
 
-	addAndMakeVisible(levelSlider);
-	addAndMakeVisible(levelLabel);
-
-	setSize(600, 100);
-
+	label_level_.setText("Noise Level", NotificationType::dontSendNotification);
+	addAndMakeVisible(label_level_);
 	// Make sure you set the size of the component after
 	// you add any child components.
 	setSize(800, 600);
 
 	// Some platforms require permissions to open input channels so request that here
-	if (juce::RuntimePermissions::isRequired(juce::RuntimePermissions::recordAudio)
-		&& ! juce::RuntimePermissions::isGranted(juce::RuntimePermissions::recordAudio))
+	if (RuntimePermissions::isRequired(RuntimePermissions::recordAudio) && !RuntimePermissions::isGranted(
+		RuntimePermissions::recordAudio))
 	{
-		juce::RuntimePermissions::request(juce::RuntimePermissions::recordAudio,
-		                                  [&](bool granted) { setAudioChannels(granted ? 2 : 0, 2); });
+		RuntimePermissions::request(RuntimePermissions::recordAudio,
+		                            [&](bool granted) { if (granted) setAudioChannels(2, 2); });
 	}
 	else
 	{
-		// Specify the number of input and output channels that we want to open
-		setAudioChannels(2, 2);
+		setAudioChannels(1, 2);
 	}
 }
 
 MainComponent::~MainComponent()
 {
-	// This shuts down the audio device and clears the audio source.
 	shutdownAudio();
 }
 
@@ -46,48 +41,39 @@ MainComponent::~MainComponent()
 void MainComponent::prepareToPlay(int samplesPerBlockExpected, double sampleRate)
 {
 	auto t = deviceManager.getCurrentAudioDevice()->getName();
-	juce::String message;
-	message << "Preparing to play audio..." << juce::newLine;
-	message << " samplesPerBlockExpected = " << samplesPerBlockExpected << juce::newLine;
+	String message;
+	message << "Preparing to play audio..." << newLine;
+	message << " samplesPerBlockExpected = " << samplesPerBlockExpected << newLine;
 	message << " sampleRate = " << sampleRate;
-	juce::Logger::getCurrentLogger()->writeToLog(message);
+	Logger::getCurrentLogger()->writeToLog(message);
 }
 
-void MainComponent::getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill)
+void MainComponent::getNextAudioBlock(const AudioSourceChannelInfo& bufferToFill)
 {
-	bufferToFill.clearActiveBufferRegion();
 	auto* device = deviceManager.getCurrentAudioDevice();
 	auto activeInputChannels = device->getActiveInputChannels();
 	auto activeOutputChannels = device->getActiveOutputChannels();
 	auto maxInputChannels = activeInputChannels.getHighestBit() + 1;
 	auto maxOutputChannels = activeOutputChannels.getHighestBit() + 1;
-
-	auto level = (float)levelSlider.getValue();
+	// std::cout << "maxOutputChannels: " << maxOutputChannels;
+	auto sldrLevel = (float)slider_noise_level_.getValue();
 
 	for (auto channel = 0; channel < maxOutputChannels; ++channel)
 	{
-		if ((!activeOutputChannels[channel]) || maxInputChannels == 0)
-		{
+		if (!activeOutputChannels[channel] || maxInputChannels == 0)
 			bufferToFill.buffer->clear(channel, bufferToFill.startSample, bufferToFill.numSamples);
-		}
 		else
 		{
-			auto actualInputChannel = channel % maxInputChannels; // [1]
-
-			if (!activeInputChannels[channel]) // [2]
-			{
+			auto actualInputChannel = channel % maxInputChannels;
+			if (!activeInputChannels[channel])
 				bufferToFill.buffer->clear(channel, bufferToFill.startSample, bufferToFill.numSamples);
-			}
-			else // [3]
+			else
 			{
-				auto *inBuffer = bufferToFill.buffer->getReadPointer(actualInputChannel, bufferToFill.startSample);
-				auto *outBuffer = bufferToFill.buffer->getWritePointer(channel, bufferToFill.startSample);
-
+				auto* inputBuffer = bufferToFill.buffer->getReadPointer(actualInputChannel, bufferToFill.startSample);
+				auto* outputBuffer = bufferToFill.buffer->getWritePointer(channel, bufferToFill.startSample);
 				for (auto sample = 0; sample < bufferToFill.numSamples; ++sample)
 				{
-					float next_random = random.nextFloat();
-						outBuffer[sample] = inBuffer[sample] * level;
-					int x = 5;
+					outputBuffer[sample] = inputBuffer[sample] * sldrLevel;
 				}
 			}
 		}
@@ -98,21 +84,18 @@ void MainComponent::releaseResources()
 {
 	// This will be called when the audio device stops, or when it is being
 	// restarted due to a setting change.
-
-	// For more details, see the help for AudioProcessor::releaseResources()
 }
 
 //==============================================================================
-void MainComponent::paint(juce::Graphics& g)
+void MainComponent::paint(Graphics& g)
 {
 	// (Our component is opaque, so we must completely fill the background with a solid colour)
-	g.fillAll(getLookAndFeel().findColour(juce::ResizableWindow::backgroundColourId));
+	g.fillAll(getLookAndFeel().findColour(ResizableWindow::backgroundColourId));
 
 	// You can add your drawing code here!
 }
 
 void MainComponent::resized()
 {
-	levelLabel.setBounds(10, 10, 90, 20);
-	levelSlider.setBounds(100, 10, getWidth() - 110, 20);
+	slider_noise_level_.setBounds(10, 30, getWidth() - 60, 90);
 }
--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -1,36 +1,32 @@
 #pragma once
 
-#include <JuceHeader.h>
+#include "../JuceLibraryCode/JuceHeader.h"
+using namespace juce;
 
 //==============================================================================
 /*
     This component lives inside our window, and this is where you should put all
     your controls and content.
 */
-class MainComponent : public juce::AudioAppComponent
+class MainComponent : public AudioAppComponent
 {
 public:
-	//==============================================================================
-	MainComponent();
-	~MainComponent() override;
+  //==============================================================================
+  MainComponent();
+  ~MainComponent();
 
-	//==============================================================================
-	void prepareToPlay(int samplesPerBlockExpected, double sampleRate) override;
-	void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill) override;
-	void releaseResources() override;
+  //==============================================================================
+  void prepareToPlay(int samplesPerBlockExpected, double sampleRate) override;
+  void getNextAudioBlock(const AudioSourceChannelInfo &bufferToFill) override;
+  void releaseResources() override;
 
-	//==============================================================================
-	void paint(juce::Graphics& g) override;
-	void resized() override;
+  //==============================================================================
+  void paint(Graphics &g) override;
+  void resized() override;
 
 private:
-	juce::Random random;
-	juce::Slider levelSlider;
-	juce::Label levelLabel;
+  Slider slider_noise_level_;
+  Label label_level_;
 
-	//==============================================================================
-	// Your private member variables go here...
-
-
-	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainComponent)
+  JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainComponent)
 };
